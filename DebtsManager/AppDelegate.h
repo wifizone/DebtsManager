f19e143f5e7bb741f71935f31a9f059b5474616b
Anton Poluyanov <poluyanov80@gmail.com>
2018-01-30 19:33:45 +0300

    Initial Commit

---
+++ DebtsManager/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  DebtsManager
+//
+//  Created by Антон Полуянов on 30/01/2018.
+//  Copyright © 2018 Антон Полуянов. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
