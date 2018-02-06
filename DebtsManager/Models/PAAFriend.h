ada3acaf539594cbf5e713b6ce2122086e37b377
Anton Poluyanov <poluyanov80@gmail.com>
2018-02-06 19:02:38 +0300

    Added Friend model

---
+++ DebtsManager/Models/PAAFriend.h
@@ -0,0 +1,13 @@
+//
+//  PAAFriend.h
+//  DebtsManager
+//
+//  Created by Антон Полуянов on 06/02/2018.
+//  Copyright © 2018 Антон Полуянов. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PAAFriend : NSObject
+
+@end
---
+++ DebtsManager/Models/PAAFriend.m
@@ -0,0 +1,13 @@
+//
+//  PAAFriend.m
+//  DebtsManager
+//
+//  Created by Антон Полуянов on 06/02/2018.
+//  Copyright © 2018 Антон Полуянов. All rights reserved.
+//
+
+#import "PAAFriend.h"
+
+@implementation PAAFriend
+
+@end
